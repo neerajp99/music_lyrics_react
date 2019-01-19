6ab73aafc76a1feb8933c17c0cc949159980b032
neerajp99 <neerajp1999@gmail.com>
2019-01-19 08:52:23 +0530

    added axios get method and process.env file

--- src/components/MusicTracks/Tracks.js
+++ src/components/MusicTracks/Tracks.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-import Consumer from '../../context';
+import { Consumer } from '../../context';
 
 class Tracks extends Component {
   render() {
     return (
       <Consumer >
+      {(value) => (
+        <React.Fragment>
+          {console.log(value.state)}
+
+            <h1>Hello Wolrd</h1>
+
+
+        </React.Fragment>
+
+
+
+      )}
 
       </Consumer>
     )
--- src/context.js
+++ src/context.js
@@ -1,31 +1,40 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 const MyContext = React.createContext();
 
 class MyProvider extends Component {
   state = {
-    tracks_list: [
-      {
-        track: {
-          track_name: "abcd"
-        }
-      },
-      {
-        track: {
-          track_name: "lmakak"
-        }
-      }
-    ]
+    tracks_list: []
   };
+
+  componentDidMount = () => {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=20&country=us&f_has_lyrics=1&apikey=${
+          process.env.REACT_APP_APP_KEY
+        }`
+      )
+      .then(result => {
+        console.log(result.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   render() {
-    return <MyContext.Provider value = {{
-      state: this.state
-    }}>
-      {this.props.children}
-    </MyContext.Provider>;
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
   }
 }
-
 
 export const Consumer = MyContext.Consumer;
 export default MyProvider;
